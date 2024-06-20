67e9fa0a3a95c9ed787cb6efe42e193056ee5d63
Natalya Segal <natalya.segal@gmail.com>
2024-06-20 22:20:03 +0300

    Update train_a_cycle_gan.py

--- train_a_cycle_gan.py
+++ train_a_cycle_gan.py
@@ -3,13 +3,30 @@ import sys
 import os
 # Append the directory containing split.py to the path
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
+from torchvision import datasets, transforms#, models
+from torch.utils.data import DataLoader
 from models.cycle_gan.adaptive_cycle_gan_pt import config_a_cgan, train_a_cgan
 
 def main(args):
   config_a_cgan config
   config.num_epochs = args.epochs   
   config.texture_loss_weight = args.texture_loss_weight
-  train_a_cgan()
+
+  # Transformation to convert images to grayscale and normalize them
+  transform = transforms.Compose([
+      transforms.Grayscale(num_output_channels=1),  # Convert to grayscale
+      transforms.Resize((256, 256)),
+      transforms.ToTensor(),
+      transforms.Normalize((0.5,), (0.5,))  # Normalize for 1 channel
+  ])
+  
+  # Load datasets
+  dataset_A = datasets.ImageFolder(args.from_dataset_path, transform=transform)
+  dataset_B = datasets.ImageFolder(args.to_dataset_path, transform=transform)
+  dataloader_A = DataLoader(dataset_A, batch_size=1, shuffle=True)
+  dataloader_B = DataLoader(dataset_B, batch_size=1, shuffle=True)
+
+  train_a_cgan(config, dataloader_A, dataloader_B)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('')
@@ -24,6 +41,15 @@ if __name__ == '__main__':
     parser.add_argument('--random_seed',
                         help='seed for python random, used in shafling, does not affect division into train, validation and test',
                         type=int,
-                        default=9)   
+                        default=9)  
+    parser.add_argument('--from_dataset_path',
+                        help='from dataset path, dataset with the source distribution',
+                        type=str,
+                        default='./datasets/ed/A_from')
+    parser.add_argument('--to_dataset_path',
+                        help='to dataset path, dataset with the destination distribution',
+                        type=str,
+                        default='./datasets/ed/B_to/')
+
     args = parser.parse_args()
     main(args)

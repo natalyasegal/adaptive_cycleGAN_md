89478294a4596ac110987a8816e3c0bba55e37f4
Natalya Segal <natalya.segal@gmail.com>
2024-06-22 18:13:00 +0300

    Update test_cnn.py

--- test_cnn.py
+++ test_cnn.py
@@ -65,7 +65,7 @@ def main(args):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     labels, predictions, probs = test_model(model, test_loader, device)
 
-    calculate_metrics(labels, predictions, probs, th1 = 0.8)
+    calculate_metrics_test(labels, predictions, probs, th1 = 0.8)
     plot_roc_curve(labels, probs)
   
     print(f"Total samples: {len(labels)}")

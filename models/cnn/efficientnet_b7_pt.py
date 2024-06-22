a2c0613d8f803dca0a9ccb3eaf8a910a9785eeae
Natalya Segal <natalya.segal@gmail.com>
2024-06-22 17:45:26 +0300

    Update efficientnet_b7_pt.py

--- models/cnn/efficientnet_b7_pt.py
+++ models/cnn/efficientnet_b7_pt.py
@@ -4,41 +4,10 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader, random_split
 from efficientnet_pytorch import EfficientNet
-
-from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score, roc_auc_score, roc_curve, confusion_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Function to plot ROC curve
-def plot_roc_curve(labels, probs):
-    fpr, tpr, _ = roc_curve(labels, probs)
-    plt.figure()
-    plt.plot(fpr, tpr, color='blue', lw=2, label='ROC curve (area = %0.2f)' % roc_auc_score(labels, probs))
-    plt.plot([0, 1], [0, 1], color='grey', lw=2, linestyle='--')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('False Positive Rate')
-    plt.ylabel('True Positive Rate')
-    plt.title('Receiver Operating Characteristic')
-    plt.legend(loc="lower right")
-    plt.show()
-  
-# Function to calculate evaluation metrics
-def calculate_metrics(labels, predictions, probs):
-    accuracy = accuracy_score(labels, predictions)
-    balanced_acc = balanced_accuracy_score(labels, predictions)
-    f1 = f1_score(labels, predictions)
-    auc = roc_auc_score(labels, probs)
-    consusion_matrix = confusion_matrix(labels, predictions)
 
-    print(f"Accuracy: {accuracy:.4f}")
-    print(f"Balanced Accuracy: {balanced_acc:.4f}")
-    print(f"F1 Score: {f1:.4f}")
-    print(f"AUC: {auc:.4f}")
-    print(f"Confusion Matrix:\n{consusion_matrix}")
-
-    return accuracy, balanced_acc, f1, auc
-  
 # Function to prepare data
 def prepare_data(data_dir, batch_size=32, val_split=0.2):
     transform = transforms.Compose([
